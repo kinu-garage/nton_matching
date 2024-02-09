93409cd5bbe1e478af8a6365868db5679a19739a
Isaac Saito <iisaac.saito@gmail.com>
2024-02-09 18:12:50 -0500

    Fix: In test data, general role members were missing
    Signed-off-by: Isaac Saito <iisaac.saito@gmail.com>

--- n_to_n_matching/src/n_to_n_matching/test_main.py
+++ n_to_n_matching/src/n_to_n_matching/test_main.py
@@ -67,6 +67,58 @@ def test_1():
                 "child_id": 1121},
             "role_id": 2
         },
+        {
+            "id": 6,
+            "name": "guardian-name6",
+            "phone": "000-000-0000",
+            "email": "6@dot.com.dummy",
+            "children": {
+                "child_id": 116},
+            "role_id": 3
+        },
+        {
+            "id": 7,
+            "name": "guardian-name7",
+            "phone": "000-000-0000",
+            "email": "7@dot.com.dummy",
+            "children": {
+                "child_id": 1171,
+                "child_id": 1172},
+            "role_id": 3
+        },
+        {
+            "id": 8,
+            "name": "guardian-name8",
+            "phone": "000-000-0000",
+            "email": "8@dot.com.dummy",
+            "children": {
+                "child_id": 1181,
+                "child_id": 1182,
+                "child_id": 1183},
+            "role_id": 3
+        },
+        {
+            "id": 9,
+            "name": "guardian-name9",
+            "phone": "000-000-0000",
+            "email": "9@dot.com.dummy",
+            "children": {
+                "child_id": 1191,
+                "child_id": 1192},
+            "role_id": 3
+        },
+        {
+            "id": 10,
+            "name": "guardian-name10",
+            "phone": "000-000-0000",
+            "email": "10@dot.com.dummy",
+            "children": {
+                "child_id": 11101,
+                "child_id": 11102,
+                "child_id": 11103,
+                "child_id": 11104},
+            "role_id": 3
+        },
     ]
 
     num_dates = len(dates_input)
