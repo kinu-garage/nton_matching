51a0fe67c9c6a2ee4482084bcbbce22daa8e3ba6
Isaac Saito <iisaac.saito@gmail.com>
2025-08-26 10:48:29 -0400

    Update; Use master 202507

--- n_to_n_matching/src/n_to_n_matching/__main__.py
+++ n_to_n_matching/src/n_to_n_matching/__main__.py
@@ -11,11 +11,12 @@ def stdin():
     _PATH_XLSX_TEST1 = 'n_to_n_matching/test/20240602-updated_gjls_student-master.xlsx'
     _PATH_XLSX_TEST_20250322 =  "n_to_n_matching/test/2024年度_家庭名簿_当番マスタ24年度正式版_20250322_委員25年度更新.xlsx"
     _PATH_XLSX_TEST_20250503 =  "n_to_n_matching/test/20250503_gjls-family-master_mod-by-130s.xlsx"
+    _PATH_XLSX_TEST_20250726 =  "n_to_n_matching/test/20250726_gjls_student-master.xlsx"
     _SHEET_NAME = "2025当番マスター"
     _PATH_OUTIDR = '~/Desktop'
     _PATH_OUTIDR_IN_CONTAINER = "/cws/src/130s/nton_matching"
 
-    _test_path_xlsx = _PATH_XLSX_TEST_20250503
+    _test_path_xlsx = _PATH_XLSX_TEST_20250726
     parser = argparse.ArgumentParser(description=DESC_TOOL)
     parser.add_argument("-t", '--type_role', type=Roles_ID, choices=list(Roles_ID), nargs="+",
                         default=[Roles_ID.TOSHO])
